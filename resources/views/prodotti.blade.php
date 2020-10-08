7c0b21dad6489546e4505205596c54dfa01bd20b
Alex <gurievalex78@gmail.com>
2020-10-08 20:24:00 +0200

    - titolo

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -155,7 +155,6 @@ $data = '[
         <div class="cards">
             @foreach ($lunghe as $card)
                 <div class="card">
-                <h3>{{$card['titolo']}}</h3>
                     <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
                 </div>
             @endforeach
@@ -164,7 +163,6 @@ $data = '[
         <div class="cards">
             @foreach ($corte as $card)
                 <div class="card">
-                <h3>{{$card['titolo']}}</h3>
                     <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
                 </div>
             @endforeach
@@ -173,7 +171,6 @@ $data = '[
         <div class="cards">
             @foreach ($cortissime as $card)
                 <div class="card">
-                <h3>{{$card['titolo']}}</h3>
                     <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
                 </div>
             @endforeach
